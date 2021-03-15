134608ad779510cc391c743fd0f9f94f72d596ee
SUBRAHMANYA ADIGA <56469610+SUBBAADIGA@users.noreply.github.com>
2021-03-15 08:31:15 +0530

    Create Newsletter.java
    Added Java Session class in the Master branch

---
+++ src/java_sessions/Newsletter.java
@@ -0,0 +1,13 @@
+package java_sessions;
+
+public class Newsletter {
+	
+	String name;
+	int age;
+	String city;
+
+	public static void main(String[] args) {
+    
+    System.out.println("Adding Java Newsletter class");
+  }
+		
