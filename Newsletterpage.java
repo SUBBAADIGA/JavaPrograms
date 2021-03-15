87adce229f433cdaac3faa425054b1ecc15fc37b
SUBRAHMANYA ADIGA <56469610+SUBBAADIGA@users.noreply.github.com>
2021-03-15 08:27:54 +0530

    Create Newsletterpage.java
    Added newsletter page class in Java project

---
+++ Newsletterpage.java
@@ -0,0 +1,13 @@
+package java_sessions;
+
+public class Newsletterpage {
+	
+	String name;
+	int age;
+	String city;
+
+	public static void main(String[] args) {
+		System.out.println("Hello Printing name");
+		
+  }
+		
